03b4e80c45f44be2b83e71d256228ad69e02f378
vladimir.nazarov <vladimir.nazarov@corp.mail.ru>
2021-12-06 01:53:11 +0300

    UPD: als demo

--- utils/interaction_table.py
+++ utils/interaction_table.py
@@ -5,14 +5,25 @@ from scipy.sparse import csr_matrix
 from pandas.api.types import CategoricalDtype
 
 
-def clicks_weigher(clicks_df):
+def ncf_clicks_weigher(clicks_df):
     """
     (chain, user) -> interaction weight
     """
-    return None
+    clicks_df['weight'] = 1
+    clicks_df = clicks_df[['user_id', 'chain_id', 'weight']]
+    return clicks_df
 
 
-def orders_weigher(orders_df, normalize=False):
+def ncf_orders_weigher(orders_df):
+    """
+    (chain, user) -> interaction weight
+    """
+    orders_df['weight'] = 1
+    orders_df = orders_df[['user_id', 'chain_id', 'weight']]
+    return orders_df
+
+
+def orders_weigher_sum(orders_df, normalize=False):
     """
     (chain, user) -> interaction weight
     """
